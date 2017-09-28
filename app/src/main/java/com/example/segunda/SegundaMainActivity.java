15589127f631b58f8fe0bf3aa18e1a601355c6a7
zhuoIC <nicolimonar@gmail.com>
2017-09-28 13:35:12 +0200

    Calculadora funcional. Controlado los errores y cambiado a teclado numérico

--- app/src/main/java/com/example/segunda/SegundaMainActivity.java
+++ app/src/main/java/com/example/segunda/SegundaMainActivity.java
@@ -1,6 +1,5 @@
 package com.example.segunda;
 
-import android.renderscript.Double2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,50 +7,42 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
-public class SegundaMainActivity extends AppCompatActivity implements View.OnClickListener{
-    final double cambioAdolar = 1.17955;
-    final double getCambioAeuro = 0.847869;
+public class SegundaMainActivity extends AppCompatActivity implements View.OnClickListener {
+    final double getCambio = 1.17955;
     EditText etxE;
     EditText etxD;
     Button btnConvertir;
     RadioButton rbtEaD;
-    RadioButton rbtDaE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda_main);
-        EditText etxE = (EditText) findViewById(R.id.etxE);
-        EditText etxD = (EditText) findViewById(R.id.etxD);
+        etxE = (EditText) findViewById(R.id.etxE);
+        etxD = (EditText) findViewById(R.id.etxD);
         btnConvertir = (Button) findViewById(R.id.btnConvertir);
+        rbtEaD = (RadioButton) findViewById(R.id.rbtnE);
         btnConvertir.setOnClickListener(this);
     }
+
     @Override
     public void onClick(View view) {
-        if (view == btnConvertir)
-        {
+        if (view == btnConvertir) {
             actualizar();
         }
     }
 
-    public void actualizar()
-    {
-        rbtEaD = (RadioButton) findViewById(R.id.rbtnE);
+    public void actualizar() {
         double valor = 0;
-        if (rbtEaD.isChecked())
-        {
-            valor = Calcular(Double.parseDouble(etxE.getText().toString()), cambioAdolar);
-            etxE.setText(String.valueOf(valor));
-        }
-        else
-        {
-            valor = Calcular(Double.parseDouble(String.format(etxD.getText().toString())), cambioAdolar);
-            etxD.setText(String.valueOf(valor));
+        try {
+            if (rbtEaD.isChecked()) {
+                etxD.setText(String.format("%.2f", Double.parseDouble(etxE.getText().toString()) * getCambio));
+            } else {
+
+                etxE.setText(String.format("%.2f", Double.parseDouble(etxD.getText().toString()) / getCambio));
+            }
+        } catch (Exception e) {
         }
     }
 
-    public double Calcular(double valor, double cambio)
-    {
-        return valor * cambio;
-    }
 }
