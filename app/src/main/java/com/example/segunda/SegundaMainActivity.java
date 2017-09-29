bf27a62dc5ff14613d8431dbb34f97c58df7c49a
zhuoIC <nicolimonar@gmail.com>
2017-09-29 12:03:15 +0200

    Solucionado Snackbar

--- app/src/main/java/com/example/segunda/SegundaMainActivity.java
+++ app/src/main/java/com/example/segunda/SegundaMainActivity.java
@@ -3,9 +3,12 @@ package com.example.segunda;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
@@ -15,6 +18,7 @@ public class SegundaMainActivity extends AppCompatActivity implements View.OnCli
     EditText etxD;
     Button btnConvertir;
     RadioButton rbtEaD;
+    ViewGroup layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +28,18 @@ public class SegundaMainActivity extends AppCompatActivity implements View.OnCli
         etxD = (EditText) findViewById(R.id.etxD);
         btnConvertir = (Button) findViewById(R.id.btnConvertir);
         rbtEaD = (RadioButton) findViewById(R.id.rbtnE);
+        layout = (LinearLayout) findViewById(R.id.layout);
         btnConvertir.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         if (view == btnConvertir) {
-            actualizar(view);
+            actualizar();
         }
     }
 
-    public void actualizar(View view) {
+    public void actualizar() {
         double valor = 0;
         try {
             if (rbtEaD.isChecked()) {
@@ -44,8 +49,9 @@ public class SegundaMainActivity extends AppCompatActivity implements View.OnCli
                 etxE.setText(String.format("%.2f", Double.parseDouble(etxD.getText().toString()) / getCambio));
             }
         } catch (Exception e) {
+
             Toast.makeText(this, "Error al introducir datos", Toast.LENGTH_SHORT).show();
-            //Snackbar.make(view, "Error al introducir datos", Snackbar.LENGTH_SHORT).show();
+            //Snackbar.make(layout, "Error al introducir datos", Snackbar.LENGTH_SHORT).show();
         }
     }
 
